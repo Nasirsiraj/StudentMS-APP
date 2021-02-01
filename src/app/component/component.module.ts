096e69444ba65a095365111560fbe891dfe66d91
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-01 21:59:16 +0600

    error-page component created

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, ErrorPageComponent],
   imports: [
     CommonModule
   ]
