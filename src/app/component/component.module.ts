fc0abbaeb38e7850943d31921ef041ba47d2307c
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-01 22:00:29 +0600

    component module updated

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import {MaterialModule} from '../material/material.module';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, ErrorPageComponent],
+  declarations: [
+    DashboardComponent,
+    ErrorPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ]
 })
 export class ComponentModule { }
