50ae081cae8556c2b3699d64f909251106bcd4c3
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-05 12:08:08 +0600

    contact c coded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from './component/dashboard/dashboard.component';
 import {ErrorPageComponent} from './component/error-page/error-page.component';
 import {RegistrationComponent} from './component/registration/registration.component';
+import {ContactComponent} from './component/contact/contact.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent, children: [
       {path: '', redirectTo: 'registration', pathMatch: 'full'},
       {path: 'registration', component: RegistrationComponent},
+      {path: 'contact', component: ContactComponent},
       {path: '**', redirectTo: 'all-student'}
     ]},
   {path: 'error', component: ErrorPageComponent},
@@ -23,5 +25,6 @@ export class AppRoutingModule { }
 export const RoutingComponent = [
   DashboardComponent,
   ErrorPageComponent,
-  RegistrationComponent
+  RegistrationComponent,
+  ContactComponent
 ];
