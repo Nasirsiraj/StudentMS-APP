8574a58e5e140427ed8978d5e6dd40ac77b0717b
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-22 15:53:28 +0600

    find result c updated

--- src/app/component/find-result/find-result.component.ts
+++ src/app/component/find-result/find-result.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
+import {StudentService} from '../../service/student.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Result} from '../../model/result.model';
+import {ResultService} from '../../service/result.service';
 
 @Component({
   selector: 'app-find-result',
@@ -8,8 +12,15 @@ import {FormBuilder, Validators} from '@angular/forms';
 })
 export class FindResultComponent implements OnInit {
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private resultService: ResultService
   ) { }
+  result: Result | null = null
+  isFailed = false
+  isSucceed = false
+  isSubmitted = false
+  feedbackMessage = ""
 
   searchStudentForm = this.formBuilder.group({
     roll: [null, [Validators.required, Validators.min(100000), Validators.max(9999999999)]],
@@ -19,9 +30,57 @@ export class FindResultComponent implements OnInit {
   ngOnInit(): void {
   }
   onSubmit(value: any): void{
-    console.log(value)
-  }
+    this.isSubmitted = true
+    try{
+      this
+        .resultService
+        .getResultByRoll(value.roll)
+        .subscribe(
+          (response) => {
+            this.result = response
+            if(this.result == null){
+              // result not found
+              this.isFailed = true
+              this.isSucceed = false
+              this.feedbackMessage = "Error occurred!"
+              this.showSnackBar(this.feedbackMessage)
 
+            } else if(this.result != null && this.result.roll != value.roll){
+              this.isFailed = true
+              this.isSucceed = false
+              this.feedbackMessage = "Error occurred!"
+              this.showSnackBar(this.feedbackMessage)
+
+            } else if(this.result != null && this.result.roll == value.roll){
+              this.isFailed = false
+              this.isSucceed = true
+              this.feedbackMessage = "Result found!"
+              this.showSnackBar(this.feedbackMessage)
+
+            }
+          },
+          (error) => {
+            this.isFailed = true
+            this.isSucceed = false
+            this.feedbackMessage = "Server Error!"
+            this.showSnackBar(this.feedbackMessage)
+          }
+        )
+
+    }catch (e){
+      this.isFailed = true
+      this.isSucceed = false
+      this.feedbackMessage = "Error occurred!"
+      this.showSnackBar(this.feedbackMessage)
+    }
+    this.searchStudentForm.reset()
+  }
+  refreshPage(): void{
+    window.location.reload()
+  }
+  showSnackBar(message: string): void{
+    this.snackBar.open(message, 'Close', {duration: 5000})
+  }
   // getters
   get roll(){
     return this.searchStudentForm.get('roll')
