2892b810c40b700139a65b8c89ee81b45cfa624a
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-22 14:09:05 +0600

    add result form validation coded

--- src/app/component/add-result/add-result.component.ts
+++ src/app/component/add-result/add-result.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {Result} from '../../model/result.model';
 
 @Component({
   selector: 'app-add-result',
@@ -7,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddResultComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+  isSubmitted = false
+
+  addResultForm = this.formBuilder.group({
+    id: [null],
+    roll: [null, [Validators.required, Validators.min(100000), Validators.max(9999999999)]],
+    bangla: [null, [Validators.required, Validators.min(0), Validators.max(100)]],
+    english: [null, [Validators.required, Validators.min(0), Validators.max(100)]],
+    math: [null, [Validators.required, Validators.min(0), Validators.max(100)]],
+    computer: [null, [Validators.required, Validators.min(0), Validators.max(100)]],
+    physics: [null, [Validators.required, Validators.min(0), Validators.max(100)]],
+    chemistry: [null, [Validators.required, Validators.min(0), Validators.max(100)]],
+    biology: [null, [Validators.required, Validators.min(0), Validators.max(100)]],
+  })
 
   ngOnInit(): void {
   }
+  onSubmit(value: Result): void{
+    this.isSubmitted = true
+    try{
 
+
+    }catch (e){
+
+    }
+    this.addResultForm.reset()
+  }
+
+  // getters
+  get id(){
+    return this.addResultForm.get('id')
+  }
+  get roll(){
+    return this.addResultForm.get('roll')
+  }
+  get bangla(){
+    return this.addResultForm.get('bangla')
+  }
+  get english(){
+    return this.addResultForm.get('english')
+  }
+  get math(){
+    return this.addResultForm.get('math')
+  }
+  get computer(){
+    return this.addResultForm.get('computer')
+  }
+  get physics(){
+    return this.addResultForm.get('physics')
+  }
+  get chemistry(){
+    return this.addResultForm.get('chemistry')
+  }
+  get biology(){
+    return this.addResultForm.get('biology')
+  }
 }
