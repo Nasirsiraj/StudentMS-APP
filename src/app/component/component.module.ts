9b4eae480c4d2478681b5d243a3ac37be096e24a
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-22 12:53:51 +0600

    add result c created

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -4,12 +4,14 @@ import {MaterialModule} from '../material/material.module';
 import {AppRoutingModule, RoutingComponent} from '../app-routing.module';
 import { FooterComponent } from './footer/footer.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AddResultComponent } from './add-result/add-result.component';
 
 
 @NgModule({
     declarations: [
       RoutingComponent,
-      FooterComponent
+      FooterComponent,
+      AddResultComponent
     ],
     exports: [
       FooterComponent
